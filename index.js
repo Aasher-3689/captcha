6aaf50ed4d9cb2b16a0c2769e7dee70832609678
Aasher <azadpanchi3689@gmail.com>
2025-07-15 14:35:55 +0500

    minor fixes

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const defaultDesign = ["rgba(0, 136, 255, 0.211)",
-                       "rgba(0, 136, 255, 0.635)"];
-
 const correctDesign = ["rgba(0, 255, 153, 0.211)",
                        "1px solid rgba(0, 255, 153, 0.635)"];
 
@@ -9,6 +6,7 @@ const invalidDesign = ["rgba(255, 30, 0, 0.211)",
 
 const question = document.getElementById("question");
 const input = document.getElementById("input");
+const button = document.getElementById("button");
 
 function getRandomNumber() {
     return Math.floor(Math.random() * 10);
@@ -24,6 +22,8 @@ updateQuestion();
 
 async function verify(e) {
     e.preventDefault();
+    input.disabled = true;
+    button.disabled = true;
     const userAnswer = parseFloat(input.value.trim());
     const Answer = secondConstant - firstConstant;
     if (userAnswer === Answer) {
@@ -31,18 +31,19 @@ async function verify(e) {
         question.style.backgroundColor = correctDesign[0];
         question.style.border = correctDesign[1];
         await sleep(2000);
-        question.style.backgroundColor = "";
-        question.style.border = "";
-        window.open("https://aasher-3689.github.io/meritpoint", "_self")
+        window.open("https://aasher-3689.github.io/meritpoint", "_self");
+        input.disabled = "";
+        button.disabled = "";
     } else {
         input.value = "";
         question.style.backgroundColor = invalidDesign[0];
         question.style.border = invalidDesign[1];
         await sleep(2000);
-        updateQuestion();
         question.style.backgroundColor = "";
         question.style.border = "";
-
+        updateQuestion();
+        button.disabled = "";
+        input.disabled = "";
     }
 }
 
